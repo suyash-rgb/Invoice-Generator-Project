b675f5a03dc9101b2e6abfc580bcaefe77e2a96e
Suyash Baoney <suyashbaoney58@gmail.com>
2025-06-12 23:43:15 +0530

    added springboot project for backend

---
+++ invoicegeneratorapi/src/main/java/com/suyash/invoicegeneratorapi/InvoicegeneratorapiApplication.java
@@ -0,0 +1,13 @@
+package com.suyash.invoicegeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicegeneratorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicegeneratorapiApplication.class, args);
+	}
+
+}
