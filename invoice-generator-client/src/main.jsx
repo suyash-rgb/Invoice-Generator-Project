65b660a17a8f44ce7538ed157635d5dac98f7729
Suyash Baoney <suyashbaoney58@gmail.com>
2025-06-12 23:33:08 +0530

    setting up bootstrap

--- invoice-generator-client/src/main.jsx
+++ invoice-generator-client/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
