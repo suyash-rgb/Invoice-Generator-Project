12f8b1359e0f3307751e8c1b57af51cbf3e32bb7
Suyash Baoney <suyashbaoney58@gmail.com>
2025-06-13 15:52:57 +0530

    Adding Components and Pages for routing

--- invoice-generator-client/src/App.jsx
+++ invoice-generator-client/src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import LandingPage from './pages/LandingPage/LandingPage';
+import Dashboard from './pages/Dashboard';
+import MainPage from './pages/MainPage';
+import PreviewPage from './pages/PreviewPage';
+import Menubar from './components/Menubar'; 
 
 const App = () => {
   return (
-    <div className='contianer'>Application is started</div>
+    
+    <BrowserRouter>
+      <Menubar />
+      <Toaster />
+      <Routes>
+         <Route path="/" element={<LandingPage/>} />
+         <Route path="/dashboard" element={<Dashboard/>} />
+         <Route path="/generate" element={<MainPage/>} />
+         <Route path="/preview" element={<PreviewPage/>} />
+         
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ invoice-generator-client/src/assets/assets.js
@@ -0,0 +1,5 @@
+import template1 from './template1.png'
+
+export const assets = {
+   template1
+}
